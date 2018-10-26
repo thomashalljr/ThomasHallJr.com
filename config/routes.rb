689b8d5d436ff3ff1db99346e646193ec6a85fae
Thomas Hall Jr <thomashalljunior@gmail.com>
2018-10-26 15:20:51 -0400

    Simplify portfolio path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "pages#index"
-  get "pages/portfolio", to: "pages#portfolio"
+  get "portfolio", to: "pages#portfolio"
   get "resume", to: redirect("https://drive.google.com/open?id=1JP8k619YpRHOSWOhgH5nq1-8cE2iq4IOuZ1VOOdPUZ0")
 end
