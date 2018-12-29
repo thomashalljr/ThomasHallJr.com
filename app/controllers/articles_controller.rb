bdbcb0beb517150366812654c9353d43df13c033
Thomas Hall Jr <thomashalljunior@gmail.com>
2018-12-29 18:58:08 -0500

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :find_article, only: [:edit, :update, :show, :destroy]
 
   def new
     @article = Article.new
