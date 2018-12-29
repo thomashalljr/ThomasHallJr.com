bbaf357ff9ff3b66a52317567df06744f4f23b7e
Thomas Hall Jr <thomashalljunior@gmail.com>
2018-12-29 17:23:16 -0500

    Start blog creation
    All the way up to form styling.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    find_article
+  end
+
+  def edit
+    find_article
+  end
+
+  def update
+    find_article
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated"
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = "Article was not updated"
+      render "edit"
+    end
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def destroy
+    find_article
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "pages#index"
   get "portfolio", to: "pages#portfolio"
   get "resume", to: redirect("https://drive.google.com/open?id=1JP8k619YpRHOSWOhgH5nq1-8cE2iq4IOuZ1VOOdPUZ0")
+  resources :articles
+  get "blog", to: "articles#index"
 end
