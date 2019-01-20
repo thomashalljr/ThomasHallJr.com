8a41f8d553b5283346aae55032863bdbda44aec0
Thomas Hall Jr <thomashalljunior@gmail.com>
2019-01-20 14:31:36 -0500

    Redirect user login to user#show, not users#index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,13 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
      session[:user_id] = user.id
      flash[:success] = "You have logged in"
-     redirect_to users_path(user)
+     redirect_to user_path(user)
     else
      flash[:danger] = "There was something wrong with your login information"
      render "new"
     end
   end
-  
+
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have logged out"
