fc69e3aaeeb57bb40209d95b832279457a5c0dfc
Thomas Hall Jr <thomashalljunior@gmail.com>
2019-02-10 19:41:30 -0500

    Increase max length for blog post body

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
   belongs_to :user
 
   validates :title, presence: true, length: { minimum: 3,  maximum: 50 }
-  validates :body,  presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :body,  presence: true, length: { minimum: 10, maximum: 2000 }
   validates :user_id, presence: true
 end
